052ab6f89e7b4ced5978d21091eb3d31d8798a14
silvanarl <ramossilvana495@gmail.com>
2020-02-11 23:27:22 -0500

    card validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
 
+let container1 = document.getElementById("container1");
+let container2 = document.getElementById("container2");
+let container3 = document.getElementById("container3");
+let result ;
+
+container2.style.display= 'none';
+container3.style.display= 'none';
+
+let botonSí = document.getElementById("botonSí");
+let cardNumber = document.getElementById("cardNumber");
+
+botonSí.addEventListener("click",obtenerCardNumber);
+
+
+function obtenerCardNumber(){
+    alert("Que bien!");
+}
+obtenerCardNumber();
+
 console.log(validator);
