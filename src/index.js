9da08c688b208c4a0d59653b37531011c87b0f10
silvanarl <ramossilvana495@gmail.com>
2020-02-17 23:29:34 -0500

    subida nueva

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ botonSí.addEventListener("click",()=>{
 let validarCardNumber = document.getElementById("validarCardNumber");
 validarCardNumber.addEventListener("click",obtenerCardNumber);
 
-function obtenerCardNumber(){
-    let cardNumber = document.getElementById("idCardNumber").value;
+let cardNumber = document.getElementById("idCardNumber").value;
     console.log('qué es cardNumber', cardNumber);
+function obtenerCardNumber(){
     let newCardNumber= [];
     for(let i= cardNumber.length-1; i>=0;i--){
         newCardNumber.push(parseInt(cardNumber[i]));
@@ -25,6 +25,7 @@ function obtenerCardNumber(){
     console.log(newCardNumber);
 
     let newCardNumberPorDos =[];
+    let newCardNumberImpar =[];
     for(let i= 0; i<newCardNumber.length; i++){
         if(i%2!==0){
             newCardNumberPorDos.push(newCardNumber[i]*2);
@@ -34,7 +35,40 @@ function obtenerCardNumber(){
         }
     }
     console.log('Nuevo array x2 invertido',newCardNumberPorDos);
+
+    let numerosMultiplicar=[];
+    for(let i=0; i<newCardNumberPorDos.length; i++){
+        if(newCardNumberPorDos[i]>=10){
+            numerosMultiplicar.push((newCardNumberPorDos[i]-10)+1);
+        }
+        else{
+            numerosMultiplicar.push(newCardNumberPorDos[i]*1);
+        }
+    }
+    console.log(numerosMultiplicar);
+
+    const totalSuma = numerosMultiplicar.reduce((sum, current) => sum + current, 0);
+    console.log('Suma de todo es:',totalSuma);
+
+    let resultado = false;
+    if(totalSuma%10===0){
+        resultado = true;
+    }
+    console.log(resultado);
+
+    
 }
 obtenerCardNumber();
 
+function maskify(){
+    let cardNumber = document.getElementById("idCardNumber").value;
+    console.log('qué es cardNumberForMaskify', cardNumber);
+    let newCardNumberForMaskify= [];
+    for(let i=0;i<cardNumber.length;i++){
+        console.log(i);
+        newCardNumberForMaskify.push(parseInt(cardNumber[i]));
+    }
+    console.log(newCardNumberForMaskify);
+}
+maskify();
 console.log(validator);
