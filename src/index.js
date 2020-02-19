71b2baa26d4c2c45783d540c9480734323514d6f
silvanarl <ramossilvana495@gmail.com>
2020-02-18 19:04:36 -0500

    subir de nuevo

--- src/index.js
+++ src/index.js
@@ -1,74 +1,30 @@
 import validator from './validator.js';
-//import { directive } from '@babel/types';
 
 const container1 = document.getElementById("container1");
 const container2 = document.getElementById("container2");
 const container3 = document.getElementById("container3");
-let result ;
-
-//Este fragmento hace algo cuando el usuaio da click, ahora debo hacer que ese algo sea ir al cuadro te texto input
-let botonSí=document.getElementById("botonSí");
-botonSí.addEventListener("click",()=>{
-    console.log("hola");
-});
+let text;
 
 let validarCardNumber = document.getElementById("validarCardNumber");
-validarCardNumber.addEventListener("click",obtenerCardNumber);
+validarCardNumber.addEventListener("click",()=>{
+let ingresado =document.getElementById("idCardNumber").value;
+let resultado1 = validator.isvalid(ingresado);
 
-let cardNumber = document.getElementById("idCardNumber").value;
-    console.log('qué es cardNumber', cardNumber);
-function obtenerCardNumber(){
-    let newCardNumber= [];
-    for(let i= cardNumber.length-1; i>=0;i--){
-        newCardNumber.push(parseInt(cardNumber[i]));
+if(resultado1===true){
+    console.log(resultado1);
+    text = 'válida';
     }
-    console.log(newCardNumber);
-
-    let newCardNumberPorDos =[];
-    let newCardNumberImpar =[];
-    for(let i= 0; i<newCardNumber.length; i++){
-        if(i%2!==0){
-            newCardNumberPorDos.push(newCardNumber[i]*2);
-        }
-        else{
-            newCardNumberPorDos.push(newCardNumber[i]*1);
-        }
+else{
+    text = 'inválida';
     }
-    console.log('Nuevo array x2 invertido',newCardNumberPorDos);
+let valorIngresado = document.getElementById("idCardNumber").value;
+let enmascarado = validator.maskify(valorIngresado);
 
-    let numerosMultiplicar=[];
-    for(let i=0; i<newCardNumberPorDos.length; i++){
-        if(newCardNumberPorDos[i]>=10){
-            numerosMultiplicar.push((newCardNumberPorDos[i]-10)+1);
-        }
-        else{
-            numerosMultiplicar.push(newCardNumberPorDos[i]*1);
-        }
-    }
-    console.log(numerosMultiplicar);
 
-    const totalSuma = numerosMultiplicar.reduce((sum, current) => sum + current, 0);
-    console.log('Suma de todo es:',totalSuma);
+const seeResult = document.getElementById('container3');
+container3.innerHTML = `Estimado usuario su tarjeta N° ${enmascarado} es ${text}`;
+});
 
-    let resultado = false;
-    if(totalSuma%10===0){
-        resultado = true;
-    }
-    console.log(resultado);
 
-    
-}
-obtenerCardNumber();
 
-function maskify(){
-    let cardNumber = document.getElementById("idCardNumber").value;
-    console.log('qué es cardNumberForMaskify', cardNumber);
-    let newCardNumberForMaskify= [];
-    for(let i=0;i<cardNumber.length;i++){
-        console.log(i);
-        newCardNumberForMaskify.push(parseInt(cardNumber[i]));
-    }
-    console.log(newCardNumberForMaskify);
-}
-maskify();
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,57 @@
 const validator = {
+    isvalid:(creditCardNumber)=>{
+            let newCardNumber= [];
+        for(let i= creditCardNumber.length-1; i>=0;i--){
+            newCardNumber.push(parseInt(creditCardNumber[i]));
+        }
+        console.log(newCardNumber);
 
+        let newCardNumberPair =[];
+        let newCardNumberOdd =[];
+        for(let i= 0; i<newCardNumber.length; i++){
+            if(i%2!==0){
+                newCardNumberPair.push(newCardNumber[i]*2);
+            }
+            else{
+                newCardNumberPair.push(newCardNumber[i]*1);
+            }
+        }
+        console.log('Nuevo array x2 invertido',newCardNumberPair);
+
+        let multiply=[];
+        for(let i=0; i<newCardNumberPair.length; i++){
+            if(newCardNumberPair[i]>=10){
+                multiply.push((newCardNumberPair[i]-10)+1);
+            }
+            else{
+                multiply.push(newCardNumberPair[i]*1);
+            }
+        }
+        console.log(multiply);
+
+        const totalSum = multiply.reduce((sum, current) => sum + current, 0);
+        console.log('Suma de todo es:',totalSum);
+        let result = false;
+        if(totalSum%10===0){
+            result = true;
+        }
+        else{
+        }
+        return result;
+        console.log(result);
+    },
+    maskify:(creditCardNumber)=>{
+        let xmaskify = '';
+        for(let i=0 ; i<creditCardNumber.length;i++){
+            if(i<=creditCardNumber.length-5){
+                xmaskify = xmaskify + '#';
+            }
+            else{
+                xmaskify = xmaskify + creditCardNumber[i];
+            } 
+        }
+        console.log(xmaskify);
+        return xmaskify
+    }
 };
 export default validator;
