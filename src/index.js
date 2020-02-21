5bf31a06973fac47afce4c6605b28a91968c1179
silvanarl <ramossilvana495@gmail.com>
2020-02-21 12:03:12 -0500

    subida en lab

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import validator from './validator.js';
 const container1 = document.getElementById("container1");
 const container2 = document.getElementById("container2");
 const container3 = document.getElementById("container3");
+const volver = document.getElementById("texto");
 let text;
 let continueToValidation = document.getElementById("continue");
 continueToValidation.addEventListener("click",()=>{
     container2.classList.remove("ocultar");
     container1.classList.add("ocultar");
+    
 });
     
 let validateCardNumber = document.getElementById("validarCardNumber");
@@ -15,6 +17,7 @@ validateCardNumber.addEventListener("click",()=>{
 
 container3.classList.remove("ocultar");
 container2.classList.add("ocultar");
+
 let ingresado =document.getElementById("idCardNumber").value;
 if(ingresado==0){
     alert('Por favor ingresa un número de tarjeta.');
@@ -29,5 +32,13 @@ else{
     text = 'inválida';
     }
 let enmascarado = validator.maskify(ingresado);
-container3.innerHTML = `Estimado usuario su tarjeta N° ${enmascarado} es ${text}`;
+volver.innerHTML = `Estimado usuario su tarjeta N° ${enmascarado} es ${text}`;
+
+
+
+});
+const backButton = document.getElementById("buttonback");
+backButton.addEventListener("click", ()=>{
+container2.classList.remove("ocultar");
+container3.classList.add("ocultar");
 });
